d3000a5cffa2914264c8647fa2d27882548f424b
Felipe <felipeobando@estudianTEC.cr>
2021-06-13 22:56:24 -0600

    A

--- crearReportesExcel.py
+++ crearReportesExcel.py
@@ -78,7 +78,7 @@ def reporteTotalidadLicencias():#Función para crear el reporte de todas las lic
         hoja.append(licencia)
     try:
 
-        wb.save('totalidadLiencias.xlsx')#guarda el archivo con el nombre.
+        wb.save('totalidadLicencias.xlsx')#guarda el archivo con el nombre.
     except:
         return False
     return True
